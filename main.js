3ab4fba8e3bbb83be93f9d4a90f4b0aba58f9701
Efimov Maxim <maxefi@mail.ru>
2016-01-19 00:47:28 +0300

    first commit

---
+++ main.js
@@ -0,0 +1,31 @@
+(function() {
+
+  var buttons = document.getElementsByTagName('button'),
+      h1 = document.getElementsByTagName('h1'),
+      h1TextD = document.querySelector('.textD'),
+      h1TextN = document.querySelector('.textN'),
+      newH1 = document.createElement('h1'),
+      buttonDay = document.getElementById('day'),
+      contentDay = document.createTextNode('DAY'),
+      contentNight = document.createTextNode('NIGHT');
+
+  var changeDayTime = function (e) {
+    eventsObj.preventDefault(e);
+    var elem = eventsObj.getTarget(e);
+
+    if (elem.id === 'day') {
+      document.body.className = document.body.className.replace("night", "day");
+      h1TextD.className = "textD";
+      h1TextN.className = "textN none"
+    } else if (elem.id === 'night') {
+      document.body.className = document.body.className.replace("day", "night");
+      h1TextD.className = "textD none";
+      h1TextN.className = "textN"
+    }
+  };
+
+  for (var i = 0, len = buttons.length; i < len; i++) {
+    eventsObj.addEvent(buttons[i], 'click', changeDayTime);
+  }
+
+})();
